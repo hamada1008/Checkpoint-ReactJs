4eb18b87b9a90c8978bf3c6a3d94296628dac14d
hamada1008 <86958719+hamada1008@users.noreply.github.com>
2021-08-04 16:18:43 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,116 @@
+import "./styles.css";
+import { Form } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+
+const anc = (
+  <div>
+    <span> I agree to the </span>{" "}
+    <a href="javascript:void(0)"> terms and conditions</a>
+  </div>
+);
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="welcome">
+        <h2>Sign Up</h2>
+      </div>
+      {/*forms*/}
+      <div className="forms">
+        <Form>
+          <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+            <Form.Label column sm={2}>
+              Email Adress
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Control type="email" placeholder="Email Adress" />
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+            <Form.Label column sm={2}>
+              Confirm Email
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Control
+                onpaste="return false"
+                type="email"
+                placeholder="Email Confirmation"
+              />
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+            <Form.Label column sm={2}>
+              Full Name
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Control type="text" placeholder="Full Name" />
+            </Col>
+          </Form.Group>
+          <Form.Group
+            as={Row}
+            className="mb-3"
+            controlId="formHorizontalPassword"
+          >
+            <Form.Label column sm={2}>
+              Password
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Control type="password" placeholder="Password" />
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+            <Form.Label column sm={2}>
+              Confirm Password
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Control
+                type="password"
+                placeholder="Password confirmation"
+              />
+            </Col>
+          </Form.Group>
+          <fieldset>
+            <Form.Group as={Row} className="mb-3">
+              <Form.Label as="legend" column sm={2}>
+                Occupation
+              </Form.Label>
+              <Col sm={10}>
+                <Form.Check
+                  type="radio"
+                  label="Student"
+                  name="formHorizontalRadios"
+                  id="formHorizontalRadios1"
+                />
+                <Form.Check
+                  type="radio"
+                  label="Novice developper"
+                  name="formHorizontalRadios"
+                  id="formHorizontalRadios2"
+                />
+                <Form.Check
+                  type="radio"
+                  label="Expert developper"
+                  name="formHorizontalRadios"
+                  id="formHorizontalRadios3"
+                />
+              </Col>
+            </Form.Group>
+          </fieldset>
+          <Form.Group as={Row} className="mb-3" controlId="formHorizontalCheck">
+            <Col sm={{ span: 10, offset: 2 }}>
+              <Form.Check label={anc} />
+            </Col>
+          </Form.Group>
+
+          <Form.Group as={Row} className="mb-3">
+            <Col sm={{ span: 10, offset: 2 }}>
+              <Button type="submit">Sign Up</Button>
+            </Col>
+          </Form.Group>
+        </Form>
+      </div>
+    </div>
+  );
+}
